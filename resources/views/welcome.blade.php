4cbf9f4cb0bd0a64fcc7fa9b69f7afbaa8980dcb
Adedamola Ogundeinde <adedamolaogundeinde@Adedamolas-MacBook-Air.local>
2018-02-14 17:09:20 +0100

    heading change

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -57,20 +57,20 @@
 <div class="row">
   <div class="col-lg-4">
     <i class="fa fa-comments-o fa-5x" aria-hidden="true"></i>
-    <h2>Ask a Librarian</h2>
+    <h2>AIMS</h2>
     <p>Donec sed odio dui. Etiam porta sem malesuada magna mollis euismod. Nullam id dolor id nibh ultricies vehicula ut id elit. Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Praesent commodo cursus magna.</p>
     <p><a class="btn btn-default" href="#" role="button">View details &raquo;</a></p>
   </div><!-- /.col-lg-4 -->
   <div class="col-lg-4">
      
     <i class="fa fa-lightbulb-o fa-5x" aria-hidden="true"></i>
-    <h2>Innovaions</h2>
+    <h2>OBJECTIVES</h2>
     <p>Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Cras mattis consectetur purus sit amet fermentum. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh.</p>
     <p><a class="btn btn-default" href="#" role="button">View details &raquo;</a></p>
   </div><!-- /.col-lg-4 -->
   <div class="col-lg-4">
     <i class="fa fa-sign-in fa-5x" aria-hidden="true"></i>
-    <h2>Join the Library</h2>
+    <h2>CONFERENCE THEMES</h2>
     <p>Donec sed odio dui. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Vestibulum id ligula porta felis euismod semper. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.</p>
     <p><a class="btn btn-default" href="#" role="button">View details &raquo;</a></p>
   </div><!-- /.col-lg-4 -->
