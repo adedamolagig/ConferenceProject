40840254b62351439e4f76f205e1d935e55e9d2a
adedamolagig <adedamolagig@gmail.com>
2018-02-17 16:42:03 +0100

    conference organizers

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -13,24 +13,32 @@
 <!-- Three columns of text below the carousel -->
 <div class="row">
   <div class="col-lg-4">
-    <i class="fa fa-comments-o fa-5x" aria-hidden="true"></i>
+    
     <h2 style=" font-family:  Baloo Tamma; ">AIM</h2>
     <p>The aim of the conference is to bring together and integrate the stakeholders in the application of Automotive Engineering systems in Mass Transportation and Logistics operations. </p> <p>This is to provide opportunity for robust discourse, exposure, industry experience sharing & buy-in, transport systems collaborations and business development opportunities...</p>
     <p><a class="btn btn-default" href=" {{ route('aim') }} " role="button">View details &raquo;</a></p>
   </div><!-- /.col-lg-4 -->
   <div class="col-lg-4">
      
-    <i class="fa fa-lightbulb-o fa-5x" aria-hidden="true"></i>
+    
     <h2 style=" font-family:  Baloo Tamma; ">OBJECTIVES</h2>
     <p>1. Evaluate the current impact of various Automotive MEdia for an integrated Transportation system on the change Agenda of the Nation</p>
     <p>2. Consider, review and suggest improvement on all critical requirements for comprehensice national Automotive system development.
     <p><a class="btn btn-default" href="#" role="button">View details &raquo;</a></p>
   </div><!-- /.col-lg-4 -->
+
   <div class="col-lg-4">
-    <i class="fa fa-sign-in fa-5x" aria-hidden="true"></i>
+    
     <h2 style=" font-family:  Baloo Tamma; font-weight: 400; " >SUB-THEMES</h2>
     <p>1. Overview of Applications of Automotive Engines in Mass transportation</p>
     <p>2. An Overview of the Logistics systems and the impacts on economic development.</p>
+    <p><a class="btn btn-default" href=" {{ route('subThemes') }} " role="button">View details &raquo;</a></p>
+  </div><!-- /.col-lg-4 -->
+
+  <div class="col-lg-6">
+    
+    <h2 style=" font-family:  Baloo Tamma; font-weight: 400; " >Conference Organizers</h2>
+    <p>The conference is being organized by the Automotive and Locomotive Engines Division of the Nigerina Society of Engineers, in partnership with the Aeronautical Engineering and the Marine Engineering Dividions of the Society</p>
     <p><a class="btn btn-default" href=" {{ route('subThemes') }} " role="button">View details &raquo;</a></p>
   </div><!-- /.col-lg-4 -->
 </div><!-- /.row -->
