a28e51511f556cdffdf78fc5f7a418d264b78d59
adedamolagig <adedamolagig@gmail.com>
2018-02-19 15:40:23 +0100

    made changes to download folder

--- resources/views/downloads.blade.php
+++ resources/views/downloads.blade.php
@@ -7,7 +7,7 @@
 	  <img border="0" src="/images/outside.jpg" alt="flyer_outside" width="200" height="150">
 	</a>
 
-	<a href="/images/inside.jpg" download>
-	  <img border="0" src="/images/inside.jpg" alt="flyer_inside" width="200" height="150">
+	<a href="/images/inside1.jpg" download>
+	  <img border="0" src="/images/inside1.jpg" alt="flyer_inside" width="200" height="150">
 	</a>
 @endsection
