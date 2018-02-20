80ad270d054993e3ba4d9320bfbdb31c06e14b47
adedamolagig <adedamolagig@gmail.com>
2018-02-20 16:07:28 +0100

    reworking doenload page

--- resources/views/downloads.blade.php
+++ resources/views/downloads.blade.php
@@ -1,13 +1,26 @@
+
+
 @extends('layouts.app')
 
 @section('content')
-	<p>Download Conferencer Flyer:<p>
+<div class="container">
+    <div class="row">
+        <div class="">
+            <div class="panel panel-default">
+                <div class="panel-heading"><strong style="font-size: 35px; font-weight: 35px; color: green">Download Conference Flyer</strong></div>
 
-	<a href="/images/outside.jpg" download>
-	  <img border="0" src="/images/outside.jpg" alt="flyer_outside" width="200" height="150">
-	</a>
+                <div class="panel-body">
+                	<p>Click on image to download</p>
+                   	<a href="/images/outside.jpg" download>
+					  <img border="0" src="/images/outside.jpg" alt="flyer_outside" width="200" height="150">
+					</a>
 
-	<a href="/images/inside1.jpg" download>
-	  <img border="0" src="/images/inside1.jpg" alt="flyer_inside" width="200" height="150">
-	</a>
+					<a href="/images/inside.jpg" download>
+					  <img border="0" src="/images/inside.jpg" alt="flyer_inside" width="200" height="150">
+					</a>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
 @endsection
