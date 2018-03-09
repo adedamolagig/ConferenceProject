15a02a64d7acdf2258b7117e4c96714004330862
adedamolagig <adedamolagig@gmail.com>
2018-03-10 00:28:48 +0100

    edited home page to be dynamic with respective signed in user

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,7 +5,8 @@
     <div class="row">
         <div class="col-md-8 col-md-offset-2">
             <div class="panel panel-default">
-                <div class="panel-heading">Dashboard</div>
+                <div class="panel-heading"> 
+                    Dear {{ Auth::user()->salutations }} {{ Auth::user()->last_name }}, </div>
 
                 <div class="panel-body">
                     @if (session('status'))
@@ -14,7 +15,9 @@
                         </div>
                     @endif
 
-                    You are logged in!
+                    You are logged in Mega Auto conference website
+
+                    <p>Here, you can upload abstracts for the conference</p> 
                 </div>
             </div>
         </div>
