de33690c130af23a90ac9107f2d7ceef46ce157e
adedamolagig <adedamolagig@gmail.com>
2018-03-10 00:12:06 +0100

    adding salutations to the name on display

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -17,6 +17,7 @@ class CreateUsersTable extends Migration
             $table->increments('id');
             $table->string('first_name');
             $table->string('last_name');
+            // $table->string('last_name2');
             $table->string('salutations');
             $table->string('position');
             $table->string('country');
